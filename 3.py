19ba634c3e2feb3f84c8607a65bd343c11a3c712
Lochan Sokki <lochan.sokki@gmail.com>
2023-06-27 20:15:51 +0530

    i commit

--- 3.py
+++ 3.py
@@ -1,2 +1,18 @@
 my_list = [1, 'apple', True]
 print(my_list[2])
+
+
+my_list.insert(2, 2)
+
+my_list.append('lemon')
+
+
+for i in my_list:
+    print(i)
+
+
+#we can pop, remove, reverse, sort
+
+my_list.sort()
+
+print(my_list)  #error, not of same type
